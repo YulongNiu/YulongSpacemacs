ad30c3345b43b91e2dee16877011f066ff2b4410
Yulong Niu <niuylscu@gmail.com>
2017-01-07 23:22:52 +0800

    add rmarkdown support

--- yulong/packages.el
+++ yulong/packages.el
@@ -55,7 +55,29 @@
                     ("examples" . "")
                     ("author" . "Yulong Niu \\email{niuylscu@@gmail.com}"))))
             (setq ess-roxy-str "##'")
-            (setq ess-user-full-name "Yulong Niu"))))
+            (setq ess-user-full-name "Yulong Niu"))
+    (defun ess-rmarkdown ()
+      (interactive)
+      "Compile R markdown (.Rmd). Should work for any output type."
+      "http://roughtheory.com/posts/ess-rmarkdown.html"
+      ;; Check if attached R-session
+      (condition-case nil
+          (ess-get-process)
+        (error
+         (ess-switch-process)))
+      (let* ((rmd-buf (current-buffer)))
+        (save-excursion
+          (let* ((sprocess (ess-get-process ess-current-process-name))
+                 (sbuffer (process-buffer sprocess))
+                 (buf-coding (symbol-name buffer-file-coding-system))
+                 (R-cmd
+                  (format "library (rmarkdown); rmarkdown::render (\"%s\")"
+                          buffer-file-name)))
+            (message "Running rmarkdown on %s" buffer-file-name)
+            (ess-execute R-cmd 'buffer nil nil)
+            (switch-to-buffer rmd-buf)
+            (ess-show-buffer (buffer-name sbuffer) nil)))))
+))
 
 (defconst yulong-packages
   '(
